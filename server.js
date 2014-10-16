e61724f2515134932cf057694b89d456d49d28f7
jdgaffke <jdgaffke@gmail.com>
2014-10-15 20:16:50 -0700

    added

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var express = require('express');
 var router = express();
 var server = http.createServer(router);
 
-router.use(express.static(__dirname));
+router.use(express.static("static"));
 
 server.listen(process.env.PORT || 80, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("HTTP server listening at ", addr.address + ":" + addr.port);
 });
