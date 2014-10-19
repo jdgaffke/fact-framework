0748f92ede03d0fab76dba16d84419dee810d655
jdgaffke <jdgaffke@gmail.com>
2014-10-18 18:38:06 -0700

    additional

--- metadata-router.js
+++ metadata-router.js
@@ -1,16 +1,21 @@
-
 var express = require('express');
+var util = require('util');
 var router = exports.router = new express.Router({
     mergeParams: true
 });
 
-var metadal= require('./metadata.js').metadal;
+var metadata = require('./metadata.js').api;
 
 router.get("/:name", function(req, res, next) {
-    var today = 20141001;
-    metadal.getOperatorMetadata( req.params.name, today, found)
-    
-    function found(answer) {
-        res.send(answer);
+    var op = req.params.name;
+    var version = 20141001;
+    metadata.getOperatorMetadata(op, version, found);
+
+    function found(err, result) {
+        var body = err ? err : result ? result : "Unknown Error";
+        res.type('text/plain');
+        res.send(util.inspect(body, {
+            depth: null
+        }));
     }
 });
--- metadata.js
+++ metadata.js
@@ -1,18 +1,43 @@
-// Metadata processor/provider.
+/**
+ * Operator metadata processor/producer.
+ */
 
-var metadal = exports.metadal = {};
+var find = require('find'),
+    fs = require('fs'),
+    xml2js = require('xml2js');
 
-var find = require('find');
-var fs = require('fs');
+// Configuration
+var filePath = __dirname + "/operators";
+var fileRegex = /(\w+)\.(\d{8})\.xml/i;
 
-metadal.getOperatorMetadata = function(name, version, callback) {
-    var dirPath = __dirname + "/operators";
+// Exported API object.
+var api = exports.api = {};
+
+/**
+ * Reload metadata from disk, creating in-memory index.
+ */
+api.createIndex = function() {
+    throw "Unimplemented";
+};
+
+/**
+ * Lookup and return metadata for specified operator.
+ *
+ * @param name Operator name. Case sensitive.
+ * @param version Operator version. Will match the nearest preceeding version.
+ * @param callback function(err, obj) where [obj] is the in-memory meta object.
+ */
+api.getOperatorMetadata = function(name, version, callback) {
     var nameFilter = new RegExp("^" + name + ".\\d{8}.xml");
-    
-    find.file(nameFilter, dirPath, function(files) {
+    find.file(nameFilter, filePath, function(files) {
         if (files && files.length > 0) {
-            callback(fs.readFileSync(files[0]));
+            var fullPath = files[0];
+            var parser = new xml2js.Parser();
+            var xml = fs.readFileSync(fullPath);
+            parser.parseString(xml, callback);
         }
-        callback("Operator [" + name + "] not found");
+        else {
+            callback("Operator [" + name + "] not found");
+        }
     });
 };
